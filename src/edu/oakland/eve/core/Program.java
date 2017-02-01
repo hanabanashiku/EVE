94b5ed57254b59a99589068ea4525c2875b51695
beesenpai <elemaster96@aol.com>
2017-02-01 13:52:35 -0500

    Add files via upload

---
+++ src/edu/oakland/eve/core/Program.java
@@ -0,0 +1,10 @@
+package edu.oakland.eve.core;
+
+/**
+ * Created by Michael MacLean on 2/1/2017.
+ */
+public class Program {
+	public static void main(String[] args){
+
+	}
+}
---
+++ src/edu/oakland/eve/core/Settings.java
@@ -0,0 +1,48 @@
+package edu.oakland.eve.core;
+
+import java.io.*;
+
+/**
+ * @author Michael MacLean
+ * @version 1.0
+ * @since 1.0
+ * To do: resolve paths based on OS
+ */
+public class Settings implements Serializable {
+
+	private static final String filename = "settings.bin";
+
+	private Settings(){
+
+	}
+
+	/**
+	 * Load user settings
+	 * @return The settings class saved to disk, or a new settings class, or null on failure.
+	 */
+	public static Settings load(){
+		try{
+			FileInputStream fin = new FileInputStream(filename);
+			ObjectInputStream oin = new ObjectInputStream(fin);
+			Settings settings = (Settings)oin.readObject();
+			oin.close();
+			fin.close();
+			return settings;
+		}
+		catch(FileNotFoundException e)  { return new Settings(); }
+		catch(IOException|ClassNotFoundException e){ return null;}
+	}
+
+	// Called by the garbage collector
+	protected void finalize(){
+		try{
+			FileOutputStream fout = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(this);
+			out.close();
+			fout.close();
+		}
+		catch(IOException e){ }
+	}
+
+}
