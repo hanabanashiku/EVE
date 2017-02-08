b1f49ddbb6830538e609647e4da24427cf6491bd
beesenpai <elemaster96@aol.com>
2017-02-08 13:24:04 -0500

    Add files via upload
    Added Google API library
    Changed Settings.finalize() to Settings.close() - the GC in Java is not guaranteed to run!

---
+++ EVE/src/edu/oakland/eve/core/Settings.java
@@ -0,0 +1,50 @@
+package edu.oakland.eve.core;
+
+import java.io.*;
+
+/**
+ * @author Michael MacLean
+ * @version 1.0
+ * @since 1.0
+ * To do: resolve paths based on OS
+ */
+public class Settings implements Serializable {
+
+	private static final String filename = "settings.bin";
+
+	private Settings(){
+
+	}
+
+	/**
+	 * Load user settings
+	 * @return The settings class saved to disk, or a new settings class, or null on failure.
+	 */
+	public static Settings load(){
+		try{
+			FileInputStream fin = new FileInputStream(filename);
+			ObjectInputStream oin = new ObjectInputStream(fin);
+			Settings settings = (Settings)oin.readObject();
+			oin.close();
+			fin.close();
+			return settings;
+		}
+		catch(FileNotFoundException e)  { return new Settings(); }
+		catch(IOException|ClassNotFoundException e){ return null;}
+	}
+
+
+	public void close(){
+		try{
+			FileOutputStream fout = new FileOutputStream(filename);
+			ObjectOutputStream out = new ObjectOutputStream(fout);
+			out.writeObject(this);
+			out.close();
+			fout.close();
+		}
+		catch(IOException e){ }
+	}
+
+	protected void finalize(){ close(); }
+
+}
