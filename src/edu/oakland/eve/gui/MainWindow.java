44ef512ed44b44a7200e11e582d7ad7fa130bd18
Michael MacLean <elemaster96@aol.com>
2017-03-30 11:36:14 -0400

    Initial GUI push

---
+++ src/edu/oakland/eve/gui/MainWindow.java
@@ -0,0 +1,22 @@
+package edu.oakland.eve.gui;
+
+import javax.swing.*;
+
+/**
+ * @author Michael MacLean
+ * @version 1.0
+ * @since 1.0
+ */
+public class MainWindow extends JFrame{
+    private JPanel panel;
+    private JTabbedPane calendarpane;
+    private JPanel calendarpanel;
+    private JPanel rsspanel;
+    private JPanel weatherpanel;
+    private JTabbedPane tabbedPane1;
+    private JButton rsssettings;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
---
+++ src/edu/oakland/eve/gui/categoryPanel.java
@@ -0,0 +1,14 @@
+package edu.oakland.eve.gui;
+
+import javax.swing.*;
+import edu.oakland.eve.rss.Feed;
+
+/**
+ * @author Michael MacLean
+ * @version 1.0
+ * @since 1.0
+ */
+public class categoryPanel extends JPanel{
+
+
+}
